e0fe059f38b27dd5c6f6468b6012827f3497b0f6
SooBo <sjstnqh@naver.com>
2025-01-18 23:01:58 +0900

    Chore: prettier설치 및 esLint 세팅

--- eslint.config.js
+++ eslint.config.js
@@ -1,12 +1,37 @@
 import globals from "globals";
-import pluginJs from "@eslint/js";
-import pluginReact from "eslint-plugin-react";
+import js from "@eslint/js";
+import react from "eslint-plugin-react";
+import reactHooks from "eslint-plugin-react-hooks";
+import eslintConfigPrettier from "eslint-config-prettier";
+import prettierPlugin from "eslint-plugin-prettier";
 
-
-/** @type {import('eslint').Linter.Config[]} */
+/** @type {import("eslint").Linter.Config[]} */
 export default [
-  {files: ["**/*.{js,mjs,cjs,jsx}"]},
-  {languageOptions: { globals: globals.browser }},
-  pluginJs.configs.recommended,
-  pluginReact.configs.flat.recommended,
+  {ignores: ["dist/", "node_modules/", ".output/", ".wxt/", ".public/"]},
+  {
+    files: ["**/*.{js,jsx}"],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+      parserOptions: {
+        ecmaVersion: "latest",
+        ecmaFeatures: {jsx: true},
+        sourceType: "module",
+      },
+    },
+    settings: {react: {version: "19"}},
+    plugins: {
+      react,
+      "react-hooks": reactHooks,
+      "prettier": prettierPlugin,
+    },
+    rules: {
+      ...js.configs.recommended.rules,
+      ...react.configs.recommended.rules,
+      ...react.configs["jsx-runtime"].rules,
+      ...reactHooks.configs.recommended.rules,
+      "prettier/prettier": "error",
+    },
+  },
+  eslintConfigPrettier,
 ];
