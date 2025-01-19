bb1fb87c198f003d4c9b64f05d05636211ca7aa8
SooBo <sjstnqh@naver.com>
2025-01-19 20:56:54 +0900

    [개발환경] 보일러플레이트 구축

--- wxt.config.js
+++ wxt.config.js
@@ -1,7 +1,12 @@
-import { defineConfig } from "wxt";
+import {defineConfig} from "wxt";
 
 // See https://wxt.dev/api/config.html
 export default defineConfig({
   extensionApi: "chrome",
   modules: ["@wxt-dev/module-react"],
+  manifest: {
+    name: "Smooth Move",
+    description: "Smooth Move",
+    permissions: ["storage"],
+  },
 });
