28415aed0e3f539fa12dad82aff92593f4509689
SooBo <sjstnqh@naver.com>
2025-01-19 20:35:27 +0900

    Chore: React파일구조 세팅 및 import linter 플러그인 추가

--- eslint.config.js
+++ eslint.config.js
@@ -1,5 +1,6 @@
 import js from "@eslint/js";
 import eslintConfigPrettier from "eslint-config-prettier";
+import importPlugin from "eslint-plugin-import";
 import prettierPlugin from "eslint-plugin-prettier";
 import react from "eslint-plugin-react";
 import reactHooks from "eslint-plugin-react-hooks";
@@ -27,14 +28,21 @@ export default [
     plugins: {
       react,
       "react-hooks": reactHooks,
-      "prettier": prettierPlugin,
+      prettier: prettierPlugin,
+      import: importPlugin,
     },
     rules: {
       ...js.configs.recommended.rules,
       ...react.configs.recommended.rules,
       ...react.configs["jsx-runtime"].rules,
       ...reactHooks.configs.recommended.rules,
-      "prettier/prettier": "error",
+      "prettier/prettier": [
+        "error",
+        {
+          useTabs: false,
+          endOfLine: "auto",
+        },
+      ],
     },
   },
   eslintConfigPrettier,
